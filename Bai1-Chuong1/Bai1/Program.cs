4ced8de8545096969fd7eec74e55d98b9f2dc2e6
huongnguyen-512 <huong05hn1203@gmail.com>
2024-07-17 07:29:22 +0700

    bai 1 Chuong 1

---
+++ Bai1-Chuong1/Bai1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai1
+{
+    internal class Program
+        
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, d, tich;
+            Console.WriteLine("Nhap a: ");
+            //a = int.Parse(Console.ReadLine());
+            a=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Nhap b: ");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap c: ");
+            c = int.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap d: ");
+            d = int.Parse(Console.ReadLine());
+            tich = d * c * b * a;
+            Console.WriteLine($"Tich cua bon so: {tich}");
+
+        }
+    }
+}
